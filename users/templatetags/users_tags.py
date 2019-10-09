d72714e5e4ff74f9696e871d7d31936b0b9c2b09
Aqlan Nor Azman <aqlanhadi@gmail.com>
2019-10-09 22:23:47 +0800

    modified superuser access on templates

--- users/templatetags/users_tags.py
+++ users/templatetags/users_tags.py
@@ -4,11 +4,17 @@ register = template.Library()
 
 @register.filter
 def is_manager(user):
-    return user.groups.filter(name='Managers').exists()
+    if user.is_superuser:
+        return True
+    else:
+        return user.groups.filter(name='Managers').exists()
 
 @register.filter
 def is_guard(user):
-    return user.groups.filter(name='Guards').exists()
+    if user.is_superuser:
+        return True
+    else:
+        return user.groups.filter(name='Guards').exists()
 
 @register.filter(name='is_in')
 def is_in(user, group):
