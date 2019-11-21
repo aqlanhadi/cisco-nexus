91cf1cb86cd665ee95c98bdf88142157318b1fbc
Aqlan Nor Azman <aqlanhadi@gmail.com>
2019-11-21 11:56:50 +0800

    viva day

--- payroll_module/models.py
+++ payroll_module/models.py
@@ -8,9 +8,13 @@ class Salary(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(Guard, on_delete=models.CASCADE)
     pay_date = models.DateField(default=time)
-    month = models.IntegerField(max_length=2, editable=False, default=int(time.strftime('%m')))
+    month = models.IntegerField(max_length=2, editable=False, blank=True, null=True)
     base_pay = models.FloatField(max_length=32, default=0)
     bonus = models.FloatField(max_length=32, default=0)
 
     class Meta:
         verbose_name_plural = "Salaries"
+
+    def save(self, *args, **kwargs):
+        month = int(self.pay_date.strftime('%m'))
+        super(Salary, self).save(*args, **kwargs)
